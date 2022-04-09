e9e82e24fe4d53c361ffbb3abc5da2e18320d0fa
FRANCISCO USIEL PEREZ PACHECO <91095112+FranciscoPerez18@users.noreply.github.com>
2022-04-08 21:50:34 -0500

    act de voto controller

--- app/Http/Controllers/VotoController.php
+++ app/Http/Controllers/VotoController.php
@@ -8,14 +8,12 @@ use App\Models\Casilla;
 use App\Models\Eleccion;
 use App\Models\Voto;
 use App\Models\Votocandidato;
-use Exception;
 use Illuminate\Support\Facades\DB;
 
 class VotoController extends Controller
-{   
-    private $DUPLICATE_KEY_CODE=23000;
-    private $DUPLICATE_KEY_MESSAGE="Ya existe un dato igual en la BD, ". 
-            "no se permiten duplicados";
+{
+    private $DUPLICATE_KEY_CODE=2300;
+    private $DUPICATE_KEY_MESSAGE="ya existe un dato igual en la BD,". "no se permiten duplicados";
     /**
      * Display a listing of the resource.
      *
@@ -23,7 +21,7 @@ class VotoController extends Controller
      */
     public function index()
     {
-        $votos = Voto::all();
+        $votos  = Voto::all();
         return view('voto/list', compact('votos'));
     }
 
@@ -37,17 +35,7 @@ class VotoController extends Controller
         $casillas = Casilla::all();
         $candidatos = Candidato::all();
         $elecciones = Eleccion::all();
-        return view('voto/create',compact('casillas','candidatos','elecciones'));
-    }
-
-    private function validateVote($request){
-        foreach($request->all() as $key=>$value){
-            if (substr($key,0,10)=="candidato_")
-                if ($value<0){
-                    return false;
-                }
-        }
-        return true;
+        return view('voto/create', compact('casillas','candidatos','elecciones'));
     }
 
     /**
@@ -58,17 +46,13 @@ class VotoController extends Controller
      */
     public function store(Request $request)
     {
-            if (!($this->validateVote($request))){
-                return "Lo votos no pueden ser negativos";
+        $candidatos=[];
+            foreach($request->all() as $k=>$v){
+                if (substr($k,0,10)=="candidato_")
+                    $candidatos[substr($k,10)]=$v;
             }
-            $candidatos=[];
-            foreach($request->all() as $key=>$value){
-                if (substr($key,0,10)=="candidato_")
-                    $candidatos[substr($key,10)]=$value;
-            }
-
-    
-            $data['eleccion_id']=$request->eleccion_id;
+            
+        $data['eleccion_id']=$request->eleccion_id;
             $data['casilla_id']=$request->casilla_id;
             $evidenceFileName ="";
             if ($request->hasFile('evidencia')) {
@@ -77,36 +61,31 @@ class VotoController extends Controller
             if ($request->hasFile('evidencia')) $request->file('evidencia')->move(public_path('pdf'), $evidenceFileName);
 
             $data['evidencia']=$evidenceFileName;
-            
-            $message="save successfull";
-            $success=true;
+            $success=false;
+            $message="save sucessfull";
             DB::beginTransaction();
             try {
-                //--- save to voto
                 $voto =Voto::create($data);
     
                 //--- save to votocandidato
-                foreach($candidatos as $key=>$value){
+                foreach($candidatos as $k=>$v){
                     $votocandidato=[];
                     $votocandidato['voto_id']= $voto->id;
-                    $votocandidato['candidato_id'] = $key;
-                    $votocandidato['votos']=$value;
+                    $votocandidato['candidato_id'] = $k;
+                    $votocandidato['votos']=$v;
                     Votocandidato::create($votocandidato);
                 }
                 DB::commit();
-                
+                $success=true;
             } catch (\Exception $e) {
-                $success=false;
                 DB::rollback();
-                if ($e->getCode()==$this->DUPLICATE_KEY_CODE)
-                    $message=$this->DUPLICATE_KEY_MESSAGE;
+                if($e->getCode()==$this->$DUPLICATE_KEY_CODE)
+                $message=$this->$DUPICATE_KEY_MESSAGE;
                 else
-                    $message=$e->getMessage();
+                $message=$e->getMessage();
             }
-        
-        return view('message',compact('message','success'));
-        
-    }  
+            return view('message',compact('message','success'));
+    }
 
     /**
      * Display the specified resource.
@@ -127,13 +106,10 @@ class VotoController extends Controller
      */
     public function edit($id)
     {
-        $voto= Voto::find($id);
-        if ($voto){
-            return view ('voto/edit',compact('voto'));
-        } else {
-            $message= "No se localizó voto $id";
-            return view('message',$message);
-        }
+        $voto = Voto::find($id);
+        $casillas = Casilla::all();
+        $candidatos = Candidato::all();
+        return view ('voto/edit', compact('voto','casillas','candidatos')); 
     }
 
     /**
@@ -185,8 +161,6 @@ class VotoController extends Controller
         }
     
         return view('message',compact('message','success'));
-    
-       
     }
 
     /**
@@ -197,19 +171,22 @@ class VotoController extends Controller
      */
     public function destroy($id)
     {
-        DB::beginTransaction();
-        $success=true;
-        try {
-            Votocandidato::where('voto_id', '=', $id)->delete();
-            Voto::whereId($id)->delete();
-            DB::commit();
-            $message="Operacion exitosa";
+    DB::beginTransaction();
+    $success=true;
+    try{
+        Votocandidato::where('voto_id','=',$id)->delete();
+        Voto::whereId($id)->delete();
+        DB::commit();
+        $message="Operacion exitosa ";
+    }catch(\Exception $ex){
+        DB::rollBack();
+        $message=$ex->getMessage();
+        $success=false;
 
-        } catch (\Exception $ex){
-            DB::rollBack();
-            $message = $ex->getMessage();
-            $success=false;
-        }     
-        return view ('message',compact('message','success'));
     }
+    return view ('message',compact('message','success'));
+        
+    }
+    
+
 }
